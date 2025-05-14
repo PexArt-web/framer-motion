32b133d8113939d54521ec7ad59692074ce87bf7
PexArt-web <pexart74@gmail.com>
2025-05-14 16:08:24 +0100

    Framer-motion

--- pizzajoint/src/components/Base.js
+++ pizzajoint/src/components/Base.js
@@ -3,19 +3,53 @@ import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
 const containerVariant = {
-  hidden:{
-    opacity
-  }
-}
+  hidden: {
+    opacity: 0,
+    x: "100vw",
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: "spring",
+      delay: 0.5,
+    },
+  },
+};
+
+const nextVariant = {
+  hidden: {
+    x: "-100vw",
+  },
+  visible: {
+    x: 0,
+    transition: {
+      type: "spring",
+      stiffness: 120,
+    },
+  },
+};
+
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      duration: 0.3,
+      yoyo: Infinity,
+    },
+  },
+};
 const Base = ({ addBase, pizza }) => {
   const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
 
   return (
     <motion.div
       className="base container"
-      initial={{ x: "100vw" }}
-      animate={{ x: 0 }}
-      transition={{ type: "spring", delay: "0.5" }}
+      variants={containerVariant}
+      initial="hidden"
+      animate="visible"
     >
       <h3>Step 1: Choose Your Base</h3>
       <ul>
@@ -26,7 +60,7 @@ const Base = ({ addBase, pizza }) => {
               key={base}
               onClick={() => addBase(base)}
               whileHover={{ scale: 1.3, originX: 0, color: "#f8e112" }}
-              transition={{type:'spring', stiffness:300 }}
+              transition={{ type: "spring", stiffness: 300 }}
             >
               <span className={spanClass}>{base}</span>
             </motion.li>
@@ -35,20 +69,9 @@ const Base = ({ addBase, pizza }) => {
       </ul>
 
       {pizza.base && (
-        <motion.div
-          className="next"
-          initial={{ x: "-100vw" }}
-          animate={{ x: 0 }}
-          transition={{ type: "spring", stiffness: 120 }}
-        >
+        <motion.div className="next" variants={nextVariant}>
           <Link to="/toppings">
-            <motion.button
-              whileHover={{
-                scale: 1.1,
-                textShadow: "0px 0px 8px rgb(255,255,255)",
-                boxShadow: "0px 0px 8px rgb(255,255,255)",
-              }}
-            >
+            <motion.button variants={buttonVariants} whileHover="hover">
               Next
             </motion.button>
           </Link>
